c6980226472de77c51e65c2f643f5eeccfb303ec
Andrew Harker <ajharker7@gmail.com>
2018-05-19 15:22:59 -0700

    Put txtfile into array

--- person2.cpp
+++ person2.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
-#include<fstream>
-using namespace std;
+#include <iostream>
+#include <string>
+#include <fstream>
 
-int main() {
-    ifstream file;
-    file.open("questions.txt");
+int main()
+{
+    using namespace std;
+
+    ifstream file("file.txt");
+    if(file.is_open())
+    {
+        string myArray[5];
+
+        for(int i = 0; i < 5; ++i)
+        {
+            file >> myArray[i];
+        }
+    }
+
 }
