b1f61e33b03469cea612730407bee9c52df3a6c6
Daquartzinator <39445649+Daquartzinator@users.noreply.github.com>
2018-05-19 15:04:46 -0700

    Main Code

---
+++ NWAPW_TestTaker.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Type test to take the test." << endl;
+    cout << "Type create to write a test problem." << endl;
+    string userIn;
+    cin >> userIn;
+}
