5c915dc871caa833a81467d9792a1ea28651920d
Daquartzinator <39445649+Daquartzinator@users.noreply.github.com>
2018-05-19 15:58:16 -0700

    Main Code - Update 4

--- NWAPW_TestTaker.cpp
+++ NWAPW_TestTaker.cpp
@@ -3,6 +3,9 @@
 #include<vector>
 using namespace std;
 
+void displayQuestions();
+void takeTest();
+
 int main(){
     string userIn="garbage"; //Garbage value
     while(userIn != "test" and userIn != "create"){
@@ -23,6 +26,7 @@ int main(){
                 cout<<"1."<<line1[currentQuestion]<<"\n";
               }
         }
+        displayQuestions();
     }
 
     else if(userIn == "create"){
@@ -32,5 +36,69 @@ int main(){
         outfile << "Data";
         return 0;
 
+    }
+}
+
+void displayQuestions(){
+    string line;
+    ifstream myfile("Questions");
+    if(myfile.is_open()){
+        while(getline (myfile,line)){
+            cout << line << endl;
+        }
+        myfile.close();
+    }
+    else{
+        cout << "Unable to open file" << endl;
+    }
+}
+
+void takeTest(){
+
+    string answer1 = "6";
+    string answer2 = "7";
+    string answer3 = "7.6 billion";
+    string answer4 = "58.4 g/mol";
+    string answer5 = "Hola";
+
+    string input1;
+    string input2;
+    string input3;
+    string input4;
+    string input5;
+
+    cout  << "What is the answer to question 1?" << endl;
+    cin >> input1;
+
+    if(input1 == answer1){
+        cout << "Correct!" << endl;
+    }
+
+    cout << "What is the answer to question 2?" << endl;
+    cin >> input2;
+
+    if(input2 == answer2){
+        cout << "Correct!" << endl;
+    }
+
+    cout << "What is the answer to question 3?" << endl;
+    cin >> input3;
+
+    if(input3 == answer3){
+        cout << "Correct!" << endl;
+    }
+
+    cout << "What is the answer to question 4?" << endl;
+    cin >> input4;
+
+    if(input4 == answer4){
+        cout << "Correct!" << endl;
+    }
+
+    cout << "What is the answer to question 5?" << endl;
+    cin >> input5;
+
+    if(input5 == answer5){
+        cout << "Correct!" << endl;
     }
 }
