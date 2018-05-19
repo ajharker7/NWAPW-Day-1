2db2968e7bc570aeff221632c3e0e1bf37da6336
Daquartzinator <39445649+Daquartzinator@users.noreply.github.com>
2018-05-19 15:08:44 -0700

    Main code - update 1

--- NWAPW_TestTaker.cpp
+++ NWAPW_TestTaker.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    cout << "Type test to take the test." << endl;
-    cout << "Type create to write a test problem." << endl;
-    string userIn;
-    cin >> userIn;
+    string userIn="garbage"; //Garbage value
+    while(userIn != "test" and userIn != "create"){
+        cout << "Type test to take the test." << endl;
+        cout << "Type create to write a test problem." << endl;
+        cin >> userIn;
+    }
 }
